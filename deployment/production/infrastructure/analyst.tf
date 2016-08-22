bc46a3085f9f7f049b11549b1ade9a1e1779c5e3
Brian Dentino <brian.dentino@gmail.com>
2016-08-21 22:52:10 -0700

    add analyst production infrastructure

--- deployment/production/infrastructure/analyst.tf
+++ deployment/production/infrastructure/analyst.tf
@@ -1,35 +1,35 @@
-# resource "aws_security_group" "rancher-production-analyst-sg" {
-#   name = "rancher-finboxio-production-analyst-host-sg"
+resource "aws_security_group" "rancher-production-analyst-sg" {
+  name = "rancher-finboxio-production-analyst-host-sg"
 
-#   egress {
-#     from_port = 0
-#     to_port = 0
-#     protocol = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-# }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
-# module "analyst" {
-#   source = "../../../modules/host-fleet"
-#   deployment_id = "${var.deployment_id}"
-#   environment = "${var.name}"
-#   group = "analyst"
-#   type = "cattle"
+module "analyst" {
+  source = "../../../modules/host-fleet"
+  deployment_id = "${var.deployment_id}"
+  environment = "${var.name}"
+  group = "analyst"
+  type = "cattle"
 
-#   spot_pools = "${var.analyst_spot_pools}"
+  spot_pools = "${var.analyst_spot_pools}"
 
-#   cluster_size = "${var.analyst_cluster_size}"
-#   spot_allocation = "lowestPrice"
-#   spot_price = "${var.spot_price}"
-#   ssh_keypair = "${var.ssh_keypair}"
-#   shudder_sqs_url = "${var.shudder_sqs_url}"
-#   config_bucket = "${var.config_bucket}"
-#   host_security_group = "${aws_security_group.rancher-production-analyst-sg.id}"
-#   server_security_group = "${var.server_security_group}"
+  cluster_size = "${var.analyst_cluster_size}"
+  spot_allocation = "lowestPrice"
+  spot_price = "${var.spot_price}"
+  ssh_keypair = "${var.ssh_keypair}"
+  shudder_sqs_url = "${var.shudder_sqs_url}"
+  config_bucket = "${var.config_bucket}"
+  host_security_group = "${aws_security_group.rancher-production-analyst-sg.id}"
+  server_security_group = "${var.server_security_group}"
 
-#   rancher_hostname = "${var.rancher_hostname}"
-#   slack_webhook = "${var.slack_webhook}"
+  rancher_hostname = "${var.rancher_hostname}"
+  slack_webhook = "${var.slack_webhook}"
 
-#   version = "${var.version}"
-#   ami = "${var.ami}"
-# }
+  version = "${var.version}"
+  ami = "${var.ami}"
+}
